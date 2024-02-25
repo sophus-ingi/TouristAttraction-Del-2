a4ad7c90302920f456b9bd5ed3654a0720d5702b
Sophus Ingi <Sophussophusson@gmail.com>
2024-02-25 18:32:19 +0100

    Initial commit

---
+++ src/main/java/com/example/touristattractiondel2/TouristAttractionDel2Application.java
@@ -0,0 +1,13 @@
+package com.example.touristattractiondel2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TouristAttractionDel2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TouristAttractionDel2Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/touristattractiondel2/TouristAttractionDel2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.touristattractiondel2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TouristAttractionDel2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
